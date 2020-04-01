bd5c326a3475dcf2aa7d0adccf8422efe8633b56
taher romdhane <taher.romdhane@supcom.tn>
2020-04-01 21:59:06 +0100

    fixed some issues, added a "predict_proba" method as well as a separate method for printing iterations.

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,29 +4,35 @@ class LogisticRegression :
 
     def __init__(self, learning_rate = 0.005, loss='binary_crossentropy') :
 
+        # Global Initialization function
+
         self.learning_rate = learning_rate
         self.costs = []
-        self.loss = loss
+        self.loss_metric = loss
         self.history = {}
 
     # Utility functions
 
     def __sigmoid(self, z):
 
-        # This function computes the sigmoid of z, a scalar or numpy array of any size.
+        # This method computes the sigmoid of z, a scalar or numpy array of any size.
+
         s = 1 / (1 + np.exp(-z))
 
         return s
 
-    def __compute_accuracy(self, y_true, y_pred):
+    def accuracy_score(self, y_pred, y_true):
 
-        # This function computes the accuracy of predictions
+        # This method computes the accuracy of predictions
+
         return (100 - np.mean(np.abs(y_pred - y_true)) * 100)
 
-    def __compute_loss(self, y_pred, y_true) :
 
-        # This function computes the loss, given the predicted and true labels
-        if self.loss == 'binary_crossentropy' :
+    def loss(self, y_pred, y_true) :
+
+        # This method computes the loss, given the predicted and true labels
+
+        if self.loss_metric == 'binary_crossentropy' :
             cost = -((1/self.batch_size) * np.sum(y_true * np.log(y_pred) + (1-y_true) * np.log(1-y_pred)))
 
         return cost
@@ -34,6 +40,9 @@ class LogisticRegression :
     # Main methods
 
     def __initialize_parameters(self, X, y, metrics) :
+
+        # This method initializes both the weight and the bias matrices according
+        # to the input's dimensions
 
         self.dim = X.shape[0]
         self.batch_size = X.shape[1]
@@ -47,15 +56,56 @@ class LogisticRegression :
 
     def __propagate(self, X, y) :
 
+        # This method takes care of the backward propagation phase
+
+        y_pred = np.zeros((1, self.batch_size))
         y_pred = self.__sigmoid(np.dot(self.w.T, X) + self.b)
-        cost = self.__compute_loss(y_pred, y)
+        cost = self.loss(y_pred, y)
         self.dw = (1/self.batch_size) * np.dot(X, (y_pred - y).T)
         self.db = (1/self.batch_size) * np.sum(y_pred - y)
         cost = np.squeeze(cost)
 
         return cost
 
+    def __print_metrics(self, X_train, y_train, X_val, y_val, epochs, metrics) :
+
+        # This method is called when the verbosity is an integer that denotes
+        # after how many epochs we print the training and validation metrics
+
+
+        to_print = "epoch " + str(i) + " ==============\n"
+        to_print += "Cost : " + str(cost)
+
+        #if X_val and y_val :
+
+        y_pred_train = self.predict_proba(X_train)
+        y_pred_val = self.predict_proba(X_val)
+
+        if 'accuracy' in metrics :
+
+            train_accuracy = self.accuracy_score(y_pred_train, y_train)
+            self.history['train_accuracy'].append(train_accuracy)
+
+            val_accuracy = self.accuracy_score(y_pred_val, y_val)
+            self.history['val_accuracy'].append(val_accuracy)
+
+        if 'loss' in metrics :
+
+            train_loss = self.loss(y_pred_train, y_train)
+            self.history['train_loss'].append(train_loss)
+
+            val_loss = self.loss(y_pred_val, y_val)
+            self.history['val_loss'].append(val_loss)
+
+        to_print += " , "
+        to_print += ''.join([metric + " : " + str(self.history[metric][-1]) + " , " for metric in self.history.keys()])
+
+        print(to_print)
+
+
     def __optimize(self, X_train, y_train, X_val, y_val, epochs, metrics, verbose) :
+
+        # The optimize method will take care of the iterations,
 
         for i in range(epochs) :
             cost = self.__propagate(X_train, y_train)
@@ -64,39 +114,16 @@ class LogisticRegression :
             self.costs.append(cost)
 
             if verbose and i%verbose==0 :
-                if X_val and y_val :
-                    y_pred_train = predict(X_train)
-                    y_pred_val = predict(X_val)
-
-                    if 'accuracy' in metrics :
-
-                        train_accuracy = self.__compute_accuracy(y_pred_train, y)
-                        self.history['train_accuracy'].append(train_accuracy)
-
-                        val_accuracy = self.__compute_accuracy(y_pred, y)
-                        self.history['val_accuracy'].append(val_accuracy)
-
-                    if 'loss' in metrics :
-
-                        y_pred_train = predict(X_train)
-                        train_loss = self.__compute_loss(y_pred_train, y)
-                        self.history['train_loss'].append(train_loss)
-
-                        y_pred_val = predict(X_val)
-                        val_loss = self.__compute_loss(y_pred, y)
-                        self.history['val_loss'].append(val_loss)
-
-                to_print = "epoch " + str(i) + " ==============\n"
-                to_print += "Cost : " + str(cost) + " , "
-                to_print += ''.join([metric + " : " + str(self.history[metric][-1]) + " , " for metric in self.history.keys()])
-                print(to_print)
-                #print(self.w)
-                #print(self.dw)
+                self.__print_metrics()
 
 
-    def fit(self, X_train, y_train, X_val=None, y_val=None, epochs=100, metrics=[], verbose=None) :
+    def fit(self, X_train, y_train, X_val, y_val, epochs=100, metrics=[], verbose=None) :
 
-        assert (not ((X_val and not y_val) or (not X_val and y_val)))
+        # This is the method that we'll want to run to train the model
+        # it will initialize the parameters then call the optimize method which will take
+        # care of the forward and backward propagations
+
+        #assert (not ((X_val and not y_val) or (not X_val and y_val)))
         assert (epochs > 0)
 
         self.__initialize_parameters(X_train, y_train, metrics)
@@ -107,7 +134,19 @@ class LogisticRegression :
 
     def predict(self, X_test) :
 
+        # This method will return 1-hot encoded predictions for the X_test labels
+
         y_pred = np.zeros((1, self.batch_size))
         y_pred = self.__sigmoid(np.dot(self.w.T, X_test) + self.b)
-        y_rounded = np.round(y_pred)
+        y_rounded = np.round(y_pred).astype(int)
         return y_rounded
+
+
+    def predict_proba(self, X_test) :
+
+        # This method will return the probabilities of the predictions of the labels
+        # of X_test
+
+        y_pred = np.zeros((1, self.batch_size))
+        y_pred = self.__sigmoid(np.dot(self.w.T, X_test) + self.b)
+        return y_pred
