84c94afcc7985eef538c00640599a23f1e8ccb75
taher romdhane <taher.romdhane@supcom.tn>
2020-04-03 02:42:02 +0100

    Fixed the accuracy issue.

--- logistic_regression.py
+++ logistic_regression.py
@@ -33,15 +33,7 @@ class LogisticRegression :
 
         # This method computes the accuracy of predictions
 
-        if self.loss_metric == 'binary_crossentropy' :
-            return (100 - np.mean(np.abs(y_pred - y_true)) * 100)
-
-        if self.loss_metric == 'categorical_crossentropy':
-            if (len(y_pred.shape)>1 and y_pred.shape[1]>1) :
-                y_pred = np.argmax(y_pred, axis=1)
-            if (len(y_true.shape)>1 and y_true.shape[1]>1) :
-                y_true = np.argmax(y_true, axis=1)
-
+        if self.loss_metric == 'binary_crossentropy' or self.loss_metric == 'categorical_crossentropy':
             return (100 - np.mean(np.abs(y_pred != y_true)) * 100)
 
 
